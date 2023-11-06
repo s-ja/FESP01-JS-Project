0d07957bf934342175cf119b64af757d9021b10e
s-ja <dkstmdwl0615@naver.com>
2023-11-06 10:08:57 +0900

    CSS position change

--- 02.typescript/todoApp/src/index.ts
+++ 02.typescript/todoApp/src/index.ts
@@ -3,6 +3,7 @@ import App from "./App";
 const root = document.querySelector("#root");
 
 (async () => {
+    // ES2022 부터 Top-level await 기능 지원
   root!.appendChild(await App());
 })();
 console.log("TODO App", location.href);
